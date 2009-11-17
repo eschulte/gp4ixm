d50cd3e2811d4559e33b560ecd3786a94bba5e57
Eric <eschulte@pinyon.(none)>
2009-11-16 18:23:54 -0700

    big sharing

--- experimenter.rb
+++ experimenter.rb
@@ -8,16 +8,16 @@ require 'libixm/libixm.rb'
 
 # create the ixm object
 puts "initializing ixm connection"
-ixm = LibIXM.new(:sfbprog_path =>   '/Users/eschulte/bin/sfbprog', # path for sfbprog or sfbprog.exe
+ixm = LibIXM.new(:sfbprog_path =>   '/nfs/adaptive/eschulte/bin/sfbprog', # path for sfbprog or sfbprog.exe
                  :sfbprog_args =>   '',                            # additional arguments
-                 :sfbprog_device => '/dev/tty.usbserial-FTE5H1S3', # device for serial-over-usb
+                 :sfbprog_device => '/dev/ttyUSB0', # '/dev/tty.usbserial-FTE5H1S3', # device for serial-over-usb
                  :sfbprog_sketch => 'single-evolve/sketch.hex')    # sketch
 
 puts "running some experiments..."
 
 # build up all reset strings
 r_strings = ["r"]
-[["s", [0, 100]],
+[["s", [100]],
  ["m", [10]],
  ["b", [10]],].each do |key, values|
   new_strings = []
@@ -28,19 +28,19 @@ r_strings = ["r"]
   end
   r_strings = new_strings
 end
-# 4.times{ r_strings.shift }
+1.times{ r_strings.shift }
 
 # Integer(ARGV[0]).times{ r_strings.shift }
 
 # start up
 puts "starting #{(r_strings.size)} runs"
-%x{mkdir -p /Users/eschulte/Desktop/ixm-experiments}
+%x{mkdir -p /nfs/adaptive/eschulte/Desktop/ixm-experiments}
 
 # set up the reflex
-ixm.attach_reflex(/^c/) do |packet|
+ixm.attach_reflex(/c/) do |packet|
   puts packet
   if packet.match(/^c.([\.\d]+) (.*)/) # negative!!
-    print "."; STDOUT.flush;
+    # print "."; STDOUT.flush;
     $current_file << "#{Time.now - $start_time}\t#{$1}\t#{$2}\n"
     $current_file.flush
     $finished = true if Float($1) == 0
@@ -56,7 +56,7 @@ r_strings.each do |r_s|
     5.times do |c|
       print "\n\t#{r_s} run #{c} on #{goal}\n\t"; STDOUT.flush
       $current_file =
-        File.open("/Users/eschulte/Desktop/ixm-experiments/"+
+        File.open("/nfs/adaptive/eschulte/Desktop/ixm-experiments/"+
                   "#{r_s.gsub(":",".").gsub(" ","_")}_g.#{i}.#{c}.log", "w")
       $finished = false
       $start_time = Time.now
